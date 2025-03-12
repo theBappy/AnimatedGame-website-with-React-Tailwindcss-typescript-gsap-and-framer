cda959f729cc01548fc2a359dce02febd0a6ee3f
theBappy <abappy575@gmail.com>
2025-03-12 19:31:46 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Features from "./components/Features"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Story from "./components/Story"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+     <About />
+     <Features />
+     <Story /> 
+     <Contact />
+     <Footer />
+    </main>
+  )
+}
+
+export default App
